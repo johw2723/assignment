c96456366aa159441f738661c47b4e5dbe06221c
HyeonWoo Jo <johw2723@gmail.com>
2022-01-07 20:50:18 +0900

    Natural Join

---
+++ DB/week1/EntityABC 비식별.sql
@@ -0,0 +1,45 @@
+--ºñ½Äº°
+CREATE TABLE "A" (
+	"a"	VARCHAR(255)		NOT NULL
+);
+
+CREATE TABLE "B" (
+	"b"	VARCHAR(255)		NOT NULL,
+	"a"	VARCHAR(255)		NOT NULL
+);
+
+CREATE TABLE "C" (
+	"c"	VARCHAR(255)		NOT NULL,
+	"b"	VARCHAR(255)		NOT NULL
+);
+
+ALTER TABLE "A" ADD CONSTRAINT "PK_A" PRIMARY KEY (
+	"a"
+);
+
+ALTER TABLE "B" ADD CONSTRAINT "PK_B" PRIMARY KEY (
+	"b"
+);
+
+ALTER TABLE "C" ADD CONSTRAINT "PK_C" PRIMARY KEY (
+	"c"
+);
+
+ALTER TABLE "B" ADD CONSTRAINT "FK_A_TO_B_1" FOREIGN KEY (
+	"a"
+)
+REFERENCES "A" (
+	"a"
+);
+
+ALTER TABLE "C" ADD CONSTRAINT "FK_B_TO_C_1" FOREIGN KEY (
+	"b"
+)
+REFERENCES "B" (
+	"b"
+);
+
+select * from A
+natural join B
+natural join C;
+
---
+++ DB/week1/EntityABC 식별.sql
@@ -0,0 +1,50 @@
+DROP TABLE A;
+--½Äº°
+CREATE TABLE "A" (
+	"a"	VARCHAR(255)		NOT NULL
+);
+
+CREATE TABLE "B" (
+	"b"	VARCHAR(255)		NOT NULL,
+	"a"	VARCHAR(255)		NOT NULL
+);
+
+CREATE TABLE "C" (
+	"c"	VARCHAR(255)		NOT NULL,
+	"b"	VARCHAR(255)		NOT NULL,
+	"a"	VARCHAR(255)		NOT NULL
+);
+
+ALTER TABLE "A" ADD CONSTRAINT "PK_A" PRIMARY KEY (
+	"a"
+);
+
+ALTER TABLE "B" ADD CONSTRAINT "PK_B" PRIMARY KEY (
+	"b",
+	"a"
+);
+
+ALTER TABLE "C" ADD CONSTRAINT "PK_C" PRIMARY KEY (
+	"c",
+	"b",
+	"a"
+);
+
+ALTER TABLE "B" ADD CONSTRAINT "FK_A_TO_B_1" FOREIGN KEY (
+	"a"
+)
+REFERENCES "A" (
+	"a"
+);
+
+ALTER TABLE "C" ADD CONSTRAINT "FK_B_TO_C_1" FOREIGN KEY (
+	"b", "a"
+)
+REFERENCES "B" (
+	"b", "a"
+);
+
+select * from A
+natural join B
+natural join C;
+
