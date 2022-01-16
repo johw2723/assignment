0c3bb3e2ea049c18623d5dbc1afcf4650e9105b4
HyeonWoo Jo <johw2723@gmail.com>
2022-01-16 22:59:02 +0900

    Update SEQUENCE.sql

--- SEQUENCE.sql
+++ SEQUENCE.sql
@@ -3,7 +3,7 @@ CREATE TABLE EMP_SEQUENCE_TB (
      , emp_name    varchar2(30)
 );
 
--- 시퀀스 생성
+-- 시퀀스 생성으로 insert 마다 번호 추가
 CREATE SEQUENCE emp_seq
 START WITH 1
 INCREMENT BY 1
@@ -14,9 +14,11 @@ INSERT INTO EMP_SEQUENCE_TB(emp_no, emp_name) VALUES(emp_seq.NEXTVAL, 'name');
 
 select * from EMP_SEQUENCE_TB;
 
+
+-- Loop 사용으로 값 넣기
 begin 
     for i in 1..100
 	loop
-	INSERT INTO EMP_SEQUENCE_TB VALUES(emp_seq.NEXTVAL, 'name');
+	INSERT INTO EMP_SEQUENCE_TB VALUES(i, 'name');
 	END LOOP;
 end; 
